5616fc242934430fccf4e980b5de801e088beece
Егор <egorhrebtov335@gmail.com>
2025-11-27 21:42:44 +0200

    .

--- blog-frontend/src/App.js
+++ blog-frontend/src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
+import { AuthProvider } from './contexts/AuthContext';
+import { PostsProvider } from './contexts/PostsContext';
+
+import HomePage from './pages/HomePage';
+import PostPage from './pages/PostPage';
+import CreatePage from './pages/CreatePage';
+import EditPage from './pages/EditPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import ProfilePage from './pages/ProfilePage';
+import NotFoundPage from './pages/NotFoundPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <AuthProvider>
+        <PostsProvider>
+          <Routes>
+
+            <Route path="/" element={<HomePage />} />
+
+            <Route path="/posts/:id" element={<PostPage />} />
+
+            <Route
+              path="/create"
+              element={
+                <CreatePage />
+              }
+            />
+
+            <Route
+              path="/edit/:id"
+              element={
+                  <EditPage />
+              }
+            />
+
+            <Route path="/login" element={<LoginPage />} />
+
+            <Route path="/register" element={<RegisterPage />} />
+
+            <Route
+              path="/profile"
+              element={
+                  <ProfilePage />
+              }
+            />
+
+            <Route path="*" element={<NotFoundPage />} />
+
+          </Routes>
+        </PostsProvider>
+      </AuthProvider>
+    </BrowserRouter>
   );
 }
 
